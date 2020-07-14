1a07944f8ad2e18ac7fcb885d3c0a68d7bcec804
Thumamah <thumamah.ahmed@student.griffith.ie>
2020-07-14 22:55:44 +0100

    full site

---
+++ script.js
@@ -0,0 +1,79 @@
+/*
+let navLinks = document.querySelectorAll(".nav li a");
+
+for(let i = 0;i<navLinks.length;i++){
+	navLinks[i].addEventListener("click",function(){
+		if(this.nextElementSibling !=null){
+		this.nextElementSibling.classList.toggle("active");
+		}	
+	
+})
+}
+
+//function myFunction() {
+  // var element = document.body;
+   //element.classList.toggle("dark-mode");
+//}
+/*
+let menuButton = document.querySelector("#menuButton");
+
+let menu = document.querySelector(".masthead nav ul");
+
+menuButton.addEventListener("click", function(){
+	menu.classList.toggle("show");
+})
+
+
+
+ let mainNav = document.querySelector(".nav");
+let navBarToggle = document.querySelector(".menuButton");
+
+navBarToggle.addEventListener("click", function() {
+  mainNav.classList.toggle("show");
+});
+*/
+
+$(document).ready(function(){
+  $('#slideshow').slick({
+	  autoplay:true,
+	  fade:true,
+	  autoplaySpeed:800,
+	  arrows: false
+  });
+});
+	
+
+
+$(document).ready(function(){
+  $('#slideshow2').slick({
+	  autoplay:true,
+	  fade:true,
+	  autoplaySpeed:800,
+	  arrows: false
+  });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+$(window).scroll(function() {
+    if ($(this).scrollTop() >= 50) {        // If page is scrolled more than 50px
+        $('#return-to-top').fadeIn(200);    // Fade in the arrow
+    } else {
+        $('#return-to-top').fadeOut(200);   // Else fade out the arrow
+    }
+});
+$('#return-to-top').click(function() {      // When arrow is clicked
+    $('body,html').animate({
+        scrollTop : 0                       // Scroll to top of body
+    }, 1000);
+});
